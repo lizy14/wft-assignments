97d47b3883202494750b7d7d251bd6f232d94c92
Li Zhaoyang <lizy14@yeah.net>
2016-07-01 14:36:54 +0800

    homepage styling

--- homepage/source/script/index.js
+++ homepage/source/script/index.js
@@ -1,8 +1,4 @@
 (function () {
-	console.log('bar');
-	var name_of_variable_x;
-	var y;
-	if (name_of_variable_x === y) {
-		console.log('foo');
-	}
+	'use strict';
+	//nothing to be done here
 })();
