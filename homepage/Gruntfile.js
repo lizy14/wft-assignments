b1ce29e52b11f4c378861d72f46d1b832dcc80e0
Li Zhaoyang <lizy14@yeah.net>
2016-07-01 21:47:24 +0800

    Minify HTML

--- homepage/Gruntfile.js
+++ homepage/Gruntfile.js
@@ -28,20 +28,28 @@ module.exports = function(grunt) {
 		},
 		copy: {
 		  main: {
-
 		    files: [
 		      {expand: true,cwd: 'source',src: ['image/*'], dest: 'build/'},
-					{expand: true,cwd: 'source',src: ['index.html'], dest: 'build/'},
 		    ],
 		  },
 		},
+		minifyHtml: {
+			options: {
+				cdata: true
+			},
+			dist: {
+				files: {
+					'build/index.html': 'source/index.html'
+				}
+			}
+		}
   });
-
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	require('load-grunt-tasks')(grunt);
 
-	grunt.registerTask('default', ['copy', 'cssmin' ,'uglify']);
+	grunt.registerTask('default', ['copy', 'cssmin' ,'uglify','minifyHtml']);
 
 };
